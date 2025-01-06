0d9c1e8c2010681a9f80797d008736726f042205
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2025-01-06 20:10:04 +0000

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Tech enthusiast with a passion for Bitcoin development.",
+      title: "Tech enthusiast with a passion for Bitcoin and FOSS development.",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
